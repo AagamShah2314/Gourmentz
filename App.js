9a8d58f9f3cf61aee5f3ef8dbdbbb21c116fc6a3
AagamShah <shahaagam2314@gmail.com>
2024-06-28 22:44:45 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,29 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-//React Elements in JSX which is compiled by BABEL
-//JSX parses injected data before running it within the site in order to prevent XSS attacks
-//Functional Component
-const name = <span>Aagam</span>;
-const Title = () => <h1 className='head'>{name} React is awesome</h1>
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='logo-container'>
+                <img className='logo' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7LYtrSpVsTfA8rihqqzZfU5p2dbIpAkatbg&s' alt='logo' />
+            </div>
+            <div className='nav-items'>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
 
-const Footer = () => <p className='footer'>This is footer</p>
+const RestaurantCard = () => {
+    return (
+        <div className='res-card'>
+            <div className='res-img'>
+                <img className='res-logo' src='https://eatbook.sg/wp-content/uploads/2022/12/cauliflower-gourmetz-catering.jpg' alt='res-img' />
+            </div>
+            <div className='res-info'>
+                <h3>Meghana Foods</h3>
+                <h4>North Indian, South Indian, Chinese</h4>
+                <h5><span>4.4 stars</span><span>38 mins</span></h5>
+            </div>
+        </div>
+    );
+};
 
+const Body = () => {
+    return (
+        <div className='body'>
+            <div className='Search'>Search</div>
+            <div className='res-container'>
+                <RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard />
+                <RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard />
+                <RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard /><RestaurantCard />
+            </div>
+        </div>
+    );
 
-//Component Composition: One component in the other
-const HeadingComponent = () => (
-    <div id='container'>
-        <Title />
-        <br />
-        {name}
-        <br />
-        <h1 className='heading'>Hello Aagam</h1>
-        <Footer />
-    </div>
-);
+}
 
-//console.log(heading);
+const AppLayout = () => {
+    return (
+        <div className='app'>
+            <Header />
+            <Body />
+        </div>
+    );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<HeadingComponent />);
+root.render(<AppLayout />);
